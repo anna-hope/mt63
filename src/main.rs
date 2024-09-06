57913b91f48137489a8f8e3a67fef15a4c2d71d8
Anna Hope <hello@annahope.me>
2024-09-06 16:02:14 -0500

    Try to transform the samples

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use std::env::args;
+use std::path::PathBuf;
+
+fn main() -> anyhow::Result<()> {
+    let args = args().collect::<Vec<_>>();
+    let wav_path = args.get(1).expect("Need a path to a wave file");
+    let wav_path = wav_path.parse::<PathBuf>()?;
+
+    let (channel, sample_rate) = mt63::get_first_channel_from_wav(&wav_path)?;
+    let mut offset = 1_000_000;
+
+    let num_samples = 44;
+    let generated_samples = mt63::generate_samples(2500.0, sample_rate, num_samples);
+
+    for _ in 0..10 {
+        let samples = channel
+            .iter()
+            .skip(offset)
+            .take(num_samples)
+            .copied()
+            .collect::<Vec<_>>();
+        let (sin, cos) = samples
+            .iter()
+            .zip(generated_samples.iter())
+            .map(|(audio_sample, generated_sample)| {
+                let audio_sample = f64::from(*audio_sample);
+                (
+                    f64::from(audio_sample) * generated_sample.sin,
+                    f64::from(audio_sample) * generated_sample.cos,
+                )
+            })
+            .reduce(|(first_sin, second_sin), (first_cos, second_cos)| {
+                (first_sin + second_sin, first_cos + second_cos)
+            })
+            .expect("Got no samples");
+        let result = sin.powi(2) + cos.powi(2);
+        dbg!(result);
+        offset += samples.len();
+    }
+    // dbg!(&sin_wave);
+    Ok(())
+}
