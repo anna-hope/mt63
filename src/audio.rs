5ad0d4baf3af7bedb872baeb2caf0a486f9a9a5b
Anna Hope <hello@annahope.me>
2024-09-13 17:58:03 -0500

    fix: fix sample_rate type in generate_samples

--- src/audio.rs
+++ src/audio.rs
@@ -1,7 +1,11 @@
+use std::borrow::Cow;
+use std::ops::Sub;
 use std::path::Path;
 
 use fundsp::math;
 use fundsp::wave::Wave;
+
+const WINDOW_SIZE_SECONDS: f64 = 0.032;
 
 #[derive(Debug)]
 pub struct Sample {
@@ -10,24 +14,59 @@ pub struct Sample {
 }
 
 #[derive(Debug)]
+pub struct AudioWindow<'audio> {
+    data: Cow<'audio, [f32]>,
+    sample_rate: f64,
+}
+
+impl<'audio> AudioWindow<'audio> {
+    fn from_audio(audio: &'audio Audio) -> Self {
+        Self {
+            data: Cow::from(&audio.data),
+            sample_rate: audio.sample_rate,
+        }
+    }
+}
+
+impl<'audio> Sub<AudioWindow<'audio>> for AudioWindow<'audio> {
+    type Output = Self;
+
+    fn sub(self, rhs: AudioWindow) -> Self::Output {
+        todo!()
+    }
+}
+
+#[derive(Debug)]
 pub struct Audio {
     pub data: Vec<f32>,
-    pub sample_rate: u32,
+    pub sample_rate: f64,
 }
 
 impl Audio {
-    fn new(channel: Vec<f32>, sample_rate: u32) -> Self {
-        Self {
-            data: channel,
-            sample_rate,
-        }
+    fn new(data: Vec<f32>, sample_rate: f64) -> Self {
+        Self { data, sample_rate }
+    }
+
+    pub fn sin_cos_at_freq(&self, freq_hz: f64) -> Vec<Sample> {
+        todo!()
+    }
+
+    pub fn window_at(&self, sample_offset: usize) -> &AudioWindow {
+        todo!()
+    }
+
+    pub fn power_at(&self, freq_hz: f64, time_secs: f64) -> f64 {
+        todo!()
+    }
+
+    pub fn shift(&self) -> Self {
+        todo!()
     }
 }
 
 impl From<Wave> for Audio {
     fn from(value: Wave) -> Self {
-        let sample_rate = value.sample_rate() as u32;
-        Self::new(value.channel(0).clone(), sample_rate)
+        Self::new(value.channel(0).clone(), value.sample_rate())
     }
 }
 
@@ -36,12 +75,12 @@ pub fn get_audio(path: &Path) -> anyhow::Result<Audio> {
     Ok(Audio::from(wave))
 }
 
-pub fn generate_samples(frequency_hz: f64, sample_rate: u32, samples: usize) -> Vec<Sample> {
+pub fn generate_samples(frequency_hz: f64, sample_rate: f64, samples: usize) -> Vec<Sample> {
     let mut sin_wave = Vec::with_capacity(samples);
 
     for sample_idx in 0..samples {
         let sample_idx = sample_idx as f64;
-        let offset = sample_idx / f64::from(sample_rate);
+        let offset = sample_idx / sample_rate;
         let sin = math::sin_hz(frequency_hz, offset);
         let cos = math::cos_hz(frequency_hz, offset);
         let sample = Sample { sin, cos };
