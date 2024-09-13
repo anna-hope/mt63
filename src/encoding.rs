55c2124c356da88b6b0d3045f1400e1868f27f8b
Anna Hope <hello@annahope.me>
2024-09-13 16:43:05 -0500

    test(encoding): add hadamard tests

--- src/encoding.rs
+++ src/encoding.rs
@@ -10,3 +10,36 @@ pub fn augmented_hadamard_matrix() -> [u64; 128] {
     }
     matrix
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn hadamard_rows() {
+        let hadamard = augmented_hadamard_matrix();
+        let row_0 = hadamard[0];
+        assert_eq!(row_0, u64::MAX);
+        let row_1 = hadamard[1];
+        assert_eq!(row_1, 0xaaaa_aaaa_aaaa_aaaa);
+    }
+
+    #[test]
+    fn hadamard_rows_and() {
+        let hadamard = augmented_hadamard_matrix();
+        for (index, row) in hadamard.iter().enumerate() {
+            for (other_index, other_row) in hadamard.iter().enumerate() {
+                let bits = *row ^ *other_row;
+                if index == other_index {
+                    assert_eq!(bits.count_ones(), 0)
+                } else {
+                    assert!(
+                        bits.count_ones() >= 32,
+                        "Got {} set bits at index {index}, other index {other_index}",
+                        bits.count_ones()
+                    );
+                }
+            }
+        }
+    }
+}
