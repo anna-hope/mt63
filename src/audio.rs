ebbe83d565175cb4ff2f5bf98574c90da9a1f040
Anna Hope <hello@annahope.me>
2024-09-16 12:53:00 -0500

    feat(audio): add is_empty to Audio (thanks clippy)

--- src/audio.rs
+++ src/audio.rs
@@ -144,6 +144,10 @@ impl Audio {
     pub fn len(&self) -> usize {
         self.data.len()
     }
+
+    pub fn is_empty(&self) -> bool {
+        self.data.is_empty()
+    }
 }
 
 impl From<Wave> for Audio {
