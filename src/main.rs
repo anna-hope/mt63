a98d28084c69b9d069f69e2e63e8ac6060779d01
Rhys Hiltner <rhys.hiltner@gmail.com>
2024-09-20 18:34:59 -0700

    feat(cli): small commands to print wav contents

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,23 @@ use mt63::audio;
 
 fn main() -> anyhow::Result<()> {
     let args = args().collect::<Vec<_>>();
+
+    let cmd_mode = args.get(1).expect("need a subcommand");
+    match cmd_mode.as_str() {
+        "print-rate" => {
+            let mut args = args;
+            return print_rate(args.split_off(1));
+        }
+        "print-samples" => {
+            let mut args = args;
+            return print_samples(args.split_off(1));
+        }
+        _ => {
+            println!("unknown subcommand");
+        }
+    }
+    return Ok(());
+
     let wav_path = args.get(1).expect("Need a path to a wave file");
     let wav_path = wav_path.parse::<PathBuf>()?;
 
@@ -41,5 +58,29 @@ fn main() -> anyhow::Result<()> {
         offset += samples.len();
     }
     // dbg!(&sin_wave);
+    Ok(())
+}
+
+fn print_rate(args: Vec<String>) -> anyhow::Result<()> {
+    let wav_path = args.get(1).expect("Need a path to a wave file");
+    let wav_path = wav_path.parse::<PathBuf>()?;
+
+    let audio = audio::get_audio(&wav_path)?;
+
+    println!("{}", audio.sample_rate);
+
+    Ok(())
+}
+
+fn print_samples(args: Vec<String>) -> anyhow::Result<()> {
+    let wav_path = args.get(1).expect("Need a path to a wave file");
+    let wav_path = wav_path.parse::<PathBuf>()?;
+
+    let audio = audio::get_audio(&wav_path)?;
+
+    for sample in audio.data {
+        println!("{}", sample);
+    }
+
     Ok(())
 }
